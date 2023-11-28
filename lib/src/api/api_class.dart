ed014bc4cd26d9bd24246361d07e4aaf979b0f75
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-28 13:22:03 +0200

    added example project

--- lib/src/api/api_class.dart
+++ lib/src/api/api_class.dart
@@ -4,16 +4,107 @@ import 'package:http/http.dart' as http;
 
 class VisionCraft {
   // Create image function.
-  static Future<void> generateImage() async {}
+  static Future<List<String>?> generateImage({
+    required String apiKey,
+    required String prompt,
+    String? negativePrompt,
+    String? model,
+    String? sampler,
+    int? cfgScale,
+    int? steps,
+    int? imageCount,
+  }) async {
+    const apiUrl = "https://visioncraftapi--vladalek05.repl.co";
 
-  // Fetch generated image function.
+    // [apiKey] get your free api key on https://t.me/VisionCraft_bot by sending /Key.
+
+    const visionCraftUrl = "$apiUrl/generate";
+
+    final visionCraftData = {
+      "model": model ?? "anything_V5",
+      "sampler": sampler ?? "Euler",
+      "prompt": prompt,
+      "negative_prompt": negativePrompt ?? "Blur",
+      "image_count": imageCount ?? 1,
+      "token": apiKey,
+      "cfg_scale": cfgScale ?? 8,
+      "steps": steps ?? 30,
+    };
+
+    // try {
+    //   final response = await http.post(
+    //     Uri.parse(visionCraftUrl),
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: json.encode(visionCraftData),
+    //   );
+
+    //   // Check if the request was successful (status code 200)
+    //   if (response.statusCode == 200) {
+    //     // Extract image URL from the response
+    //     if (imageCount != null && imageCount != 1) {}
+    //     final imageUrl =
+    //         List<String>.from(json.decode(response.body)["images"]).first;
+
+    //     // Fetch and return the image as Uint8List
+    //     final Uint8List? image = await fetchImage(imageUrl);
+
+    //     return image;
+    //   } else {
+    //     print("Error generating image: ${response.statusCode}");
+    //     // Handle error based on the status code
+    //     return null;
+    //   }
+    // } catch (error) {
+    //   print("Error generating image: $error");
+    //   // Handle error as needed
+    //   return null;
+    // }
+    try {
+      final response = await http.post(
+        Uri.parse(visionCraftUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(visionCraftData),
+      );
+
+      // Check if the request was successful (status code 200)
+      if (response.statusCode == 200) {
+        // Extract image URLs from the response
+        final List<String> imageUrls =
+            List<String>.from(json.decode(response.body)["images"]);
+
+        // If imageCount is not null and not equal to 1, return the list of image URLs
+        if (imageCount != null && imageCount != 1) {
+          return imageUrls;
+        } else {
+          // If imageCount is 1 or null, fetch and return the first image as Uint8List
+          // final Uint8List? image = await fetchImage(imageUrls.first);
+          return [imageUrls.first];
+        }
+      } else {
+        print("Error generating images: ${response.statusCode}");
+        // Handle error based on the status code
+        return null;
+      }
+    } catch (error) {
+      print("Error generating images: $error");
+      // Handle error as needed
+      return null;
+    }
+  }
+
+  // Fetch generated images function.
   static Future<Uint8List?> fetchImage(String imageUrl) async {
-    final response = await http.get(Uri.parse(imageUrl));
+    try {
+      final response = await http.get(Uri.parse(imageUrl));
 
-    if (response.statusCode == 200) {
-      return Uint8List.fromList(response.bodyBytes);
-    } else {
-      print("Error fetching image: ${response.statusCode}");
+      if (response.statusCode == 200) {
+        return Uint8List.fromList(response.bodyBytes);
+      } else {
+        print("Error fetching image: ${response.statusCode}");
+        return null;
+      }
+    } catch (error) {
+      print("Error fetching image: $error");
       return null;
     }
   }
