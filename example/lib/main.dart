9b231664a11c921042a550948be707f51f5f3b04
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-28 13:34:43 +0200

    updated 🔥

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,14 +8,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+      title: 'Vision Craft Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController controller = TextEditingController();
+
+  Future<void> createImage() async {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Vision Craft Example',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 50,
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              border: Border.all(
+                width: 0.5,
+                color: Colors.grey.shade300,
+              ),
+            ),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                hintText: 'Enter query text...',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 8),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/flutter_vision_craft.dart
+++ lib/flutter_vision_craft.dart
@@ -1,1 +1,3 @@
 library flutter_vision_craft;
+
+export 'package:flutter_vision_craft/src/api/api_class.dart';
