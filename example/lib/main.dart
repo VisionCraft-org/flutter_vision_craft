dd212c9622fbadab1b68fac0a4bb8fe3c6dd5170
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-28 14:07:13 +0200

    added getModels example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// Example of image generation [HomePage]
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -32,7 +34,7 @@ class _HomePageState extends State<HomePage> {
   Uint8List? imageResult;
   bool? isLoading;
 
-  // [apiKey] get your free api key on https://t.me/VisionCraft_bot by sending /Key.
+  /// [apiKey] get your free api key on https://t.me/VisionCraft_bot by sending /Key.
   String apiKey = "f14eb9f4-de2a-4299-9557-bc8545da9d01";
 
   Future<void> createImage() async {
@@ -59,6 +61,28 @@ class _HomePageState extends State<HomePage> {
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          PopupMenuButton(
+              // add icon, by default "3 dot" icon
+              // icon: Icon(Icons.book)
+              itemBuilder: (context) {
+            return [
+              const PopupMenuItem<int>(
+                value: 0,
+                child: Text("Get Model List"),
+              ),
+            ];
+          }, onSelected: (value) {
+            if (value == 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const GetModelListExample(),
+                ),
+              );
+            }
+          }),
+        ],
       ),
       body: isLoading != null && isLoading == true
           ? const Center(
@@ -66,48 +90,134 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.blue,
               ),
             )
-          : isLoading == null
-              ? const Center(
-                  child: Text("Enter prompt to create image"),
-                )
-              : SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: Column(
-                    children: [
-                      // TextField for prompt
-                      Container(
-                        width: double.infinity,
-                        height: 50,
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        child: TextField(
-                          controller: controller,
-                          decoration: const InputDecoration(
-                            hintText: 'Enter query text...',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(left: 8),
-                          ),
-                        ),
+          : SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  // TextField for prompt
+                  Container(
+                    width: double.infinity,
+                    height: 50,
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 0.5,
+                        color: Colors.grey.shade300,
                       ),
-                      // Image result.
-                    ],
+                    ),
+                    child: TextField(
+                      controller: controller,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter query text...',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 8),
+                      ),
+                    ),
                   ),
-                ),
+                  // Image result.
+                  imageResult == null
+                      ? const Center(
+                          child: Text("Enter prompt to create image"),
+                        )
+                      : Container(
+                          height: 300,
+                          margin: const EdgeInsets.all(8.0),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: MemoryImage(imageResult!),
+                            ),
+                          ),
+                        ),
+                ],
+              ),
+            ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.create),
+        backgroundColor: Colors.blue,
         onPressed: () {
-          // Create image and set loading to true.
+          /// Create image and set loading to true [createImage()]
           isLoading = true;
           setState(() {});
           createImage();
         },
+        child: const Icon(
+          Icons.create,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+/// Example of getting models [GetModelListExample]
+class GetModelListExample extends StatefulWidget {
+  const GetModelListExample({super.key});
+
+  @override
+  State<GetModelListExample> createState() => _GetModelListExampleState();
+}
+
+class _GetModelListExampleState extends State<GetModelListExample> {
+  List<String> models = [];
+  final VisionCraft visionCraft = VisionCraft();
+
+  Future<void> getModelList() async {
+    final result = await visionCraft.getModelList();
+    models = result;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getModelList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'GetModels Example',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: models.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.blue,
+              ),
+            )
+          : ListView.builder(
+              itemCount: models.length,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(models[index]),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          /// Refresh
+          models.clear();
+          getModelList();
+          setState(() {});
+        },
+        child: const Icon(
+          Icons.refresh,
+          color: Colors.white,
+        ),
       ),
     );
   }
