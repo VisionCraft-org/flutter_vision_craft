9264bb3e2b1d0228bad2f66d35902783a1ff90c5
Karlmathuthu <karlmathuthu11@gmail.com>
2023-11-28 12:38:31 +0200

    added getModelList function 🔥🔥

--- lib/src/api/api_class.dart
+++ lib/src/api/api_class.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
@@ -5,7 +6,7 @@ class VisionCraft {
   // Create image function.
   static Future<void> generateImage() async {}
 
-  // Fetch image function.
+  // Fetch generated image function.
   static Future<Uint8List?> fetchImage(String imageUrl) async {
     final response = await http.get(Uri.parse(imageUrl));
 
@@ -13,8 +14,30 @@ class VisionCraft {
       return Uint8List.fromList(response.bodyBytes);
     } else {
       print("Error fetching image: ${response.statusCode}");
-      // Handle error based on the status code
       return null;
+    }
+  }
+
+  // Get VisionCraft model List.
+  static Future<List<String>> getModelList() async {
+    const apiUrl = "https://visioncraftapi--vladalek05.repl.co/models";
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      // Check if the request was successful (status code 200)
+      if (response.statusCode == 200) {
+        // Extract and return the list of models
+        final List<String> models =
+            List<String>.from(json.decode(response.body));
+        return models;
+      } else {
+        print("Error fetching models: ${response.statusCode}");
+        return [];
+      }
+    } catch (error) {
+      print("Error fetching models: $error");
+      return [];
     }
   }
 }
