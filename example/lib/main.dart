f76ed3c1e8dee1402cb3dd12b15804a3a8fee40b
Karlmathuthu <karlmathuthu11@gmail.com>
2024-01-11 12:46:08 +0200

    api key hidden

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _HomePageState extends State<HomePage> {
   bool? isLoading;
 
   /// [apiKey] get your free api key on https://t.me/VisionCraft_bot by sending /Key.
-  String apiKey = "f14eb9f4-de2a-4299-9557-bc8545da9d01";
+  String apiKey = "f14eb9f4---";
 
   Future<void> createImage() async {
     String prompt = controller.text.trim().toString();
